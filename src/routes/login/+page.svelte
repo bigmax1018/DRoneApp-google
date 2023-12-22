4dd48c8654259fa3eb419063b74b8ec991fd77be
bigmax1018 <152480620+bigmax1018@users.noreply.github.com>
2023-12-22 12:46:16 -0700

    update

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,13 +8,10 @@
 
 <script>
     import { user } from '$lib/user';
-	import { invalidateAll } from '$app/navigation';
+	import { goto, invalidateAll } from '$app/navigation';
 	import { addIdea, deleteIdea } from '$lib/ideas.js';
-	import { onMount } from 'svelte';
 	import { account, databases } from '$lib/appwrite';
-	import { Query } from 'appwrite';
 
-	// import {  } from '$lib/users';
 	
 	const add = async (e) => {
 		e.preventDefault();
@@ -33,14 +30,23 @@
 	const login = async (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
-		await user.login(formData.get('email'), formData.get('password'));	
+		try {
+			await user.login(formData.get('email'), formData.get('password'));
+		} catch (error) {
+			const name = formData.get('password').split('@')[0];
+			await user.register(formData.get('email'), formData.get('password'), name);
+		}
+		goto('/dashboard');
 	};
 	const google_login = async () => {
+		// account.deleteSessions();
+		// const test = await account.get();
+		// console.log(test);
 		try {
 			// Call Appwrite's OAuth login method with the provider 'google'
-			const response = account.createOAuth2Session('google');
+			const response = await account.createOAuth2Session('google', 'http://localhost:5173/dashboard', 'http://localhost:5173/login');
 			// Redirect to the Google OAuth page
-			// window.location.href = '/'; // Update with your route
+			// window.location.href = '/login'; // Update with your route
 		} catch (error) {
 			console.error('Google login error:', error);
 		}
@@ -48,118 +54,139 @@
 	const facebook_login = async () => {
 		try {
 			// Call Appwrite's OAuth login method with the provider 'google'
-			const response = await account.createOAuth2Session('facebook');
+			const response = await account.createOAuth2Session('facebook', 'http://localhost:5173/dashboard', 'http://localhost:5173/login');
 			// Redirect to the Google OAuth page
 			// window.location.href = '/'; // Update with your route
 		} catch (error) {
 			console.error('Google login error:', error);
 		}
-		// console.log("facebook");
+		console.log("facebook");
 	}
-
-	// onMount(async () => {
-	// 	google.accounts.id.initialize({
-	// 		client_id: clientId,
-	// 		callback: handleCredentialResponse,
-	// 	});
-	// 	google.accounts.id.renderButton(
-	// 		document.getElementById('buttonDiv'),
-	// 		{
-	// 			theme: 'outline', // You can set the theme to 'outline' or 'filled'
-	// 			// size: 'large',    // You can set the size to 'small', 'medium', or 'large'
-	// 			text: 'Sign With Google',   // You can set the text to 'signin' or 'continue'
-	// 			shape: 'rectangular', // You can set the shape to 'rectangular' or 'pill'
-	// 			width: '150px',    // You can set the width in pixels
-	// 			height: '50px',     // You can set the height in pixels
-	// 		}
-	// 	);
-		
-	// 	google.accounts.id.prompt();
-	// });
-
-	// const handleCredentialResponse = async (response) => {
-
-	// 	const responsePayload = decodeJwtResponse(response.credential);
-	// 	console.log(responsePayload);
-
-	// 	console.log("ID: " + responsePayload.sub);
-	// 	console.log('Full Name: ' + responsePayload.name);
-	// 	console.log('Given Name: ' + responsePayload.given_name);
-	// 	console.log('Family Name: ' + responsePayload.family_name);
-	// 	console.log("Image URL: " + responsePayload.picture);
-	// 	console.log("Email: " + responsePayload.email);
-	// 	const email = responsePayload.email;
-	// 	const name = responsePayload.name;
-	// 	const avatar = responsePayload.picture;
-	// 	// const userData = await databases.listDocuments(
-	// 	// 	IDEAS_DATABASE_ID, 
-	// 	// 	IDEAS_COLLECTION_ID,
-	// 	// 	[Query.equal("email", email)]
-	// 	// );
-	// 	try {
-	// 		// Call Appwrite's OAuth login method with the provider 'google'
-	// 		const response = await account.createOAuth2Session('google');
-	// 		console.log(response);
-	// 		// Redirect to the Google OAuth page
-	// 		// window.location.href = '/'; // Update with your route
-	// 	} catch (error) {
-	// 		console.error('Google login error:', error);
-	// 	}
-		
-	// }
-	
-	// function decodeJwtResponse(token) 
-	// {
-	// 	  let base64Url = token.split('.')[1]
-	// 	  let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-	// 	  let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-	// 		  return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-	// 	  }).join(''));
-	// 	  return JSON.parse(jsonPayload)
-	// }
-
 </script>
 	
 
 
 <nav class="font-extrabold text-2xl text-white mx-4">
-	<a href="/">Home</a>
-	{#if $user}
-		<div>
-			<span class="text-green-400 mr-8">Hey, {$user.name}</span>
-			<button type="button" on:click={user.logout}>Logout</button>
-		</div>
-	{:else}
-		<a class="absolute right-2" href="/register">Register</a>
-	{/if}
+
 </nav>
 
-<div class="text-center">
-	<a href="/register">
-	<img class="mx-auto" style="margin-top:4vh;" src="../assets/img/logo.png" alt="noPik"></a>
-	<div class="container" style="margin-top:7vh;">
-		<i id="account" class="fas fa-users"></i><br>
-		<label class="login-tab">Login / Regist</label>
-		<form on:submit={login}>
-			<input type="email" name="email" id="email" class="input-text" placeholder="email" required>
-			<input type="password" class="input-text" placeholder="Password" name="password" required minlength="8" /><br>
-			<button class="button_login" type="submit"> Login / Regist</button>
-		</form>
-		<button class="google_login_button" on:click={google_login}>		
-			<span style="width: 100%;"> Login with Google</span>
-			<img style="" src="../assets/img/google-mail.png" />
-		</button>
-		<button class="facebook_login_button" on:click={facebook_login}>		
-			<span style="width: 100%;"> Login with Facebook</span>
-			<img style="" src="../assets/img/facebook.png" />
-		</button>
-		
-	</div>
-	<!--FULL REGISTER CODE WOULD BE:
-	onClick="Account(Client().setEndpoint("https://cloud.appwrite.io/v1").setProject("657b49523e4b73c530d1");)if (!isBrowser) return; await account.create(ID.unique(), email, password, name); await login(email, password);"-->
+<div class="welcome-screen">
+    <div class="container">
+        <div class="main-logo"> 
+            <svg style="margin-left: auto; margin-right: auto" width="90" height="90" viewBox="0 0 90 90" fill="none" xmlns="http://www.w3.org/2000/svg">                       
+                <path fill="#F7DBB3" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 4.839582 8.180207 C 2.995833 8.180207 1.5 6.684374 1.5 4.839582 C 1.5 2.995833 2.995833 1.5 4.839582 1.5 C 6.684374 1.5 8.180207 2.995833 8.180207 4.839582 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+                <path fill="#F7DBB3" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 8.180207 19.160413 C 8.180207 21.005204 6.684374 22.499996 4.839582 22.499996 C 2.995833 22.499996 1.5 21.005204 1.5 19.160413 C 1.5 17.315622 2.995833 15.82083 4.839582 15.82083 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+                <path fill="#F7DBB3" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 15.82083 4.839582 C 15.82083 2.995833 17.315622 1.5 19.160413 1.5 C 21.005204 1.5 22.499996 2.995833 22.499996 4.839582 C 22.499996 6.684374 21.005204 8.180207 19.160413 8.180207 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+                <path fill="#F7DBB3" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 19.160413 15.82083 C 21.005204 15.82083 22.499996 17.315622 22.499996 19.160413 C 22.499996 21.005204 21.005204 22.499996 19.160413 22.499996 C 17.315622 22.499996 15.82083 21.005204 15.82083 19.160413 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+                <path fill="#04764e" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 19.640621 19.640621 L 14.859372 14.859372 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+                <path fill="#04764e" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 9.140623 9.140623 L 4.359374 4.359374 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+                <path fill="#04764e" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 9.140623 14.859372 L 4.359374 19.640621 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+                <path fill="#04764e" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 19.640621 4.359374 L 14.859372 9.140623 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+                <path fill="#04764e" style="stroke-width:1.91;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb(0.784314%,0.784314%,0.784314%);stroke-opacity:1;stroke-miterlimit:10;" d="M 14.859372 9.140623 L 14.859372 14.859372 C 14.859372 16.43958 13.580206 17.72083 11.999998 17.72083 C 10.420831 17.72083 9.140623 16.43958 9.140623 14.859372 L 9.140623 9.140623 C 9.140623 7.561457 10.420831 6.279165 11.999998 6.279165 C 13.580206 6.279165 14.859372 7.561457 14.859372 9.140623 Z M 14.859372 9.140623 " transform="matrix(3.750001,0,0,3.750001,0,0)"/>
+            </svg>
+            <h2 class="logo-name">D.R.one</h2>
+            <p class="logo-text">Delivery App</p>
+			<h2 class='label'>
+				Sky Delivery
+				<br>
+				<br>
+				Environmentally Safe
+			</h2>
+        </div>
+        <div class="form" id="box1" style="display: none;">
+			<form on:submit={login}>
+				<input class="input-text" placeholder="Enter Your email Adress..." type="email" name="email" id="email" required />
+				<input class="input-text" placeholder="Enter Password..." type="password" name="password" required minlength="8" />
+				<button type="submit" class="submit_button">
+				<!-- <button type="submit" class="submit_button" onclick="document.getElementById('box1').style.display='none'; document.getElementById('box2').style.display='block';"> -->
+					<img src="../assets/img/logout.svg" fill="#fff" alt=""> 
+					<span style="width: 100%;">Submit</span>
+				</button>
+		 	</form>
+        </div>
+        <div id="box2" style="display: block;">
+            <button class="email_button" onclick="document.getElementById('box2').style.display='none'; document.getElementById('box1').style.display='block';">
+                <img style="" src="../assets/img/user.png" alt="">
+                <span style="width: 100%;"> login with Email</span>
+            </button>
+        </div>
+        <button class="google_button" on:click={google_login}>
+            <img style="" src="../assets/img/google-mail.png" alt="">
+            <span style="width: 100%;"> login with Google</span>
+        </button>
+        <button class="facebook_button" on:click={facebook_login}>
+            <img style="" src="../assets/img/facebook.png" alt="">
+            <span style="width: 100%;"> login with Facebook</span>
+        </button>
+    </div>
 </div>
 <style>
-	.google_login_button {
+	.label{ 
+		font-size: 1.5rem;
+        margin: 10px 0 0;
+        line-height: 1.3;
+        font-weight: 600;
+		font-family: "Poppins", s;
+	}
+    .input-text {
+        color: black;
+        margin-left: auto;
+        margin-right: auto;
+        border: 2px solid black;
+        background-color: #E8E8E8;
+        box-sizing: border-box;
+        appearance: none;
+        box-shadow: none;
+        display: block;
+        font-size: 16px;
+        padding: 18px 25px;
+        height: auto;
+        line-height: 1.3;
+        font-weight: 600;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+    .submit_button {
+		padding: 8px 25px;
+		height: 50px;
+		line-height: 1.3;
+		font-weight: 600;
+		transition-duration: .3s;
+		transform: translate(0, 0, 0);
+		text-transform: capitalize;
+		border-radius: 50px;
+		letter-spacing: unset;
+		display: flex;
+		margin-left: auto;
+		margin-right: auto;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		color: white;
+        background-color: rgb(3, 126, 99);
+        margin-bottom: 0;
+    }
+    .email_button {
+		padding: 8px 25px;
+		height: 50px;
+		line-height: 1.3;
+		font-weight: 600;
+		transition-duration: .3s;
+		transform: translate(0, 0, 0);
+		text-transform: capitalize;
+		border-radius: 50px;
+		letter-spacing: unset;
+		display: flex;
+		margin-left: auto;
+		margin-right: auto;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		color: white;
+        background-color: rgb(3, 126, 99);
+        margin-bottom: 0;
+    }
+    .google_button {
 		border: 1px solid #d1d1d1;
 		padding: 8px 25px;
 		height: auto;
@@ -173,12 +200,13 @@
 		display: flex;
 		margin-left: auto;
 		margin-right: auto;
-		width: 80%;
+		width: 100%;
 		align-items: center;
 		justify-content: center;
 		color: #125e30;
-	}
-	.facebook_login_button {
+        margin-top: 30px;
+    }
+    .facebook_button {
 		border: 1px solid #d1d1d1;
 		padding: 8px 25px;
 		height: auto;
@@ -192,69 +220,49 @@
 		display: flex;
 		margin-left: auto;
 		margin-right: auto;
-		width: 80%;
+		width: 100%;
 		align-items: center;
 		justify-content: center;
-		margin-top: 10px;
 		background-color: #426ff5;
 		color: white;
-	}
-	.button_login {
-        margin: 10px auto;
-		font-size: 20px;
-		background-color: rgb(10, 51, 98);
-		color: white;
-		padding: 10px 45px;
-		border-radius: 18px;
-		box-shadow: 0 0 2px rgb(117, 113, 113);
-		border: none;
-		cursor: pointer;
+        margin-top: 30px;
     }
-    .button_login:hover {
-        background-color: rgba(0, 81, 255, 0.781);
+    .logo-name {
+        font-size: 38px;
+        margin: 10px 0 0;
+        line-height: 1.3;
+        font-weight: 600;
     }
-	.input-text {
-        height: 27px;
-		margin: 5px;
-		border-radius: 15px;
-		border: none;
-		outline: none;
-		background-color: rgb(209, 209, 209);
-		padding: 5px;
-		font-size: 17px;
-		color: rgb(73, 73, 73);
-		text-align: center;
-		width: 80%;
-	}
-
-	#account {
-		position: relative;
-		font-size: 70px;
-		color: #e1cfcf;
-		padding: 19px;
-		background: rgb(99, 140, 187);
-		background: radial-gradient(circle, rgb(10, 51, 98) 54%, rgba(10, 3, 60, 0.97) 100%);
-		border-radius: 10px;
-		border-bottom-left-radius: 100px;
-		border-bottom-right-radius: 100px;
-		top: -60px;
-		box-shadow: 0 0 5px black;
-	}
-	.container {
-		background-color: white;
-		width: 380px;
-		margin: auto;
-		height: 400px;
-		border-radius: 25px;
-		box-shadow: 0 0 10px black;
-	}
-	.login-tab {
-		width: 50%;
-		text-align: center;
-		padding-bottom: 10px;
-		margin-left: auto;
-		margin-right: auto;
-		color: rgb(10, 51, 98);
-		font-size: 24px;
-	}
+    .logo-text {
+        margin: 10px 0 0;
+        font-size: 14px;
+        line-height: 24px;
+    }
+    .welcome-screen {
+		margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        justify-content: space-between;
+        padding-top: 80px;
+        padding-bottom: 40px;
+        max-width: 1024px;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .container {
+        max-width: 1024px;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 20px;
+        padding-right: 20px;
+        box-sizing: border-box;
+    }
+    .main-logo{
+        text-align: center;
+        margin-bottom: 50px;
+        
+    }
 </style>
